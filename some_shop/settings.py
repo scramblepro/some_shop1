b9f45f1bb85c0bb04223e2cce44446a4c30a99dd
Dr.Dre <zaderjkizpnet@gmail.com>
2024-09-06 23:55:44 +0300

    debug_toolbar_setup

--- some_shop/settings.py
+++ some_shop/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "debug_toolbar",
+
     'main',
     'goods'
 ]
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'some_shop.urls'
@@ -125,7 +129,11 @@ STATICFILES_DIRS: list = [
     BASE_DIR / 'static'
     ]
 
-
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
