6ef90dd784b40ecc8c8be75c73a79e8b85f3336c
Dr.Dre <zaderjkizpnet@gmail.com>
2024-10-08 23:12:28 +0300

    slugs done, prepare pagination

--- goods/views.py
+++ goods/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from django.template import context
 
 from goods.models import Products
 
 # Create your views here.
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug)) 
 
     context = {
         'title': 'Hohome - КатАлог каталОг ка ко коу коу',
@@ -14,5 +17,12 @@ def catalog(request):
     }
     return render(request, 'goods/catalog.html', context) 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+    
+    context = {
+        'product': product
+    }
+
+    return render(request, 'goods/product.html', context=context)
