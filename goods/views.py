a22fb066e3b74e4e003ec4cb6e287f39078bcaa2
Dr.Dre <zaderjkizpnet@gmail.com>
2024-10-14 01:30:05 +0300

    pagination fixed

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 from django.template import context
 
@@ -6,14 +7,19 @@ from goods.models import Products
 # Create your views here.
 def catalog(request, category_slug):
 
+    page = request.GET.get('page', 1)
+
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug)) 
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(int(page))
 
     context = {
         'title': 'Hohome - КатАлог каталОг ка ко коу коу',
-        'goods': goods,
+        'goods': current_page,
     }
     return render(request, 'goods/catalog.html', context) 
 
