cc280401691dc9d005630a5d92b35143adfadd05
Dr.Dre <zaderjkizpnet@gmail.com>
2024-11-07 01:15:59 +0300

    filters added and tested

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,4 @@
+from tkinter.messagebox import NO
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 from django.template import context
@@ -8,11 +9,19 @@ from goods.models import Products
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
     
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
@@ -20,6 +29,7 @@ def catalog(request, category_slug):
     context = {
         'title': 'Hohome - КатАлог каталОг ка ко коу коу',
         'goods': current_page,
+        'slug_url': category_slug
     }
     return render(request, 'goods/catalog.html', context) 
 
