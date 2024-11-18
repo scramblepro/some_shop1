751f3026003376ce40d70831acf73692c6ea2acf
Dr.Dre <zaderjkizpnet@gmail.com>
2024-11-19 00:57:08 +0300

    setting bd, make migrations

--- goods/utils.py
+++ goods/utils.py
@@ -8,13 +8,15 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
-
-    q_objects = Q()
-
-
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    return Products.objects.filter(description__search=query)
     
-    return Products.objects.filter(q_objects)
+    # keywords = [word for word in query.split() if len(word) > 2]
+
+    # q_objects = Q()
+
+
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    
+    # return Products.objects.filter(q_objects)
